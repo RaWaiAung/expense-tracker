10d3f0bbc06f3391cb7f4747518b0dfe6c95e042
RWADEV25 <rwadev25@gmail.com>
2026-09-12 17:49:50 +0630

    check log

--- frontend/src/utils/axiosInstance.ts
+++ frontend/src/utils/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 const API_URL = import.meta.env.VITE_API_URL;
 
-
+console.log(API_URL);
 const axiosInstance = axios.create({
   baseURL: API_URL, // Replace with your API base URL
   timeout: 10000, // Set a timeout of 10 seconds
